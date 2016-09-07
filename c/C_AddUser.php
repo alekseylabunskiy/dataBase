141b9c09648fbd748ab64b0a1914368341244248
alekseylabunskiy <aleksey_labunskiy@mail.ru>
2016-09-07 15:28:51 +0300

    gitignore M_Config

--- c/C_AddUser.php
+++ c/C_AddUser.php
@@ -18,26 +18,27 @@ class C_AddUser extends C_Base
     {
         // C_Base.
         parent::OnInput();
-        if ($this->user == null && $this->needLogin == true){
+        if ($this->user == null && $this->needLogin == true) {
             header('location:index.php?c=login');
         }
-        //создаем
+
+        //создаем пользователя
 
         if (isset($_POST['create_user'])) {
             $this->name = $this->mFunctions->Clean($_POST['new_user_name']);
             $this->email = $this->mFunctions->Clean($_POST['new_user_email']);
             $this->password = md5($_POST['new_user_password']);
 
-            $this->mUser->createUser($this->name, $this->email, $this->password, 4, 1,'avatar.png');
+            $this->mUser->createUser($this->name, $this->email, $this->password, 4, 1, 'avatar.png');
             //выбираем id новго пользователя
             $id = $this->mysqli->Select("SELECT MAX(users.user_id) AS user_id FROM users");
             //передаем на страницу просмотра профиля
             if (!isset($_GET['id'])) {
                 $_GET['id'] = $id[0]['user_id'];
             }
+
             //Перенапрвляем на страницу просмотра профиля
             header("location:index.php?c=view&id={$_GET['id']}");
-
         }
     }
 
